dcbd3664c601cfba86db8517d89e72bf9d5bd944
V ranjith <ranjithsvelmurugan@gmail.com>
2023-06-08 18:19:41 +0530

    prototype

--- src/components/login.js
+++ src/components/login.js
@@ -4,6 +4,18 @@ import login from '../img/log.png';
 import logo from '../img/logo.png';
 
 const LoginPage = () => {
+        const handleLogin = (e) => {
+          e.preventDefault();
+          // Handle login logic
+        };
+      
+        const handleGoogleLogin = () => {
+          // Handle Google login logic
+        };
+      
+        const handleAppleLogin = () => {
+          // Handle Apple ID login logic
+        };
   return (
     <div className="login-page">
         <div className="left-section">
@@ -17,7 +29,23 @@ const LoginPage = () => {
             </div>
         </div>
         <div className="right-section">
-        {/* Content for the right div */}
+            <h1 className="text3">Login in your Account</h1>
+        <form onSubmit={handleLogin}>
+            <div className="form-Container">
+                <input className="lbl" type="email" name="email" required placeholder="Email"/>
+                <br></br>
+            <   input className="lbl" type="password" name="password" required placeholder="Password"/>
+                <a href='/' className="anchor">Forgot password?</a>
+                <button type="submit" className="btn">Login</button>
+            </div>
+            
+        </form>
+        <h3 className="text4">OR</h3>
+        <div className="form-Container">
+            <button onClick={handleGoogleLogin} className="btn2">Login with Google</button>
+            <button onClick={handleAppleLogin} className="btn2">Login with Apple ID</button>
+            <p className="text4">Don't have an account? <a href ="/">Signup</a></p>
+        </div>
         </div>
     </div>
   );
